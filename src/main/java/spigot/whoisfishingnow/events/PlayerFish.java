55b359d664ada4f6d1bd2faf6b8fb09ddf405d55
VadimTotok <112573798+VadimTotok@users.noreply.github.com>
2023-05-12 16:18:09 +0300

    Update PlayerFish.java

--- src/main/java/spigot/whoisfishingnow/events/PlayerFish.java
+++ src/main/java/spigot/whoisfishingnow/events/PlayerFish.java
@@ -5,14 +5,14 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerFishEvent;
 
-public class PlayerFish implements Listener {
+public class BlockBreak implements Listener {
 
     @EventHandler
     public void onFish(PlayerFishEvent event) {
         if (event.getState().equals(PlayerFishEvent.State.CAUGHT_FISH)) {
             int x = event.getHook().getLocation().getBlockX();
-            int y = event.getHook().getLocation().getBlockX();
-            int z = event.getHook().getLocation().getBlockX();
+            int y = event.getHook().getLocation().getBlockY();
+            int z = event.getHook().getLocation().getBlockZ();
 
             event.getPlayer().getServer().getConsoleSender().sendMessage(
                     ChatColor.GREEN + "[WhoIsFishingNow] " +
